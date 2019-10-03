7f438f1d99208e38ef8d24dccdb89c0d85b47087
sarra <sarra.bouras1@gmail.com>
2019-10-03 22:27:25 +0200

    programme du jeu fonctionnel

--- 03_stairway.rb
+++ 03_stairway.rb
@@ -1,34 +1,36 @@
 
-$marche=0
-$var=1
 def de
  puts "Entre un chiffre compris entre 1 et 6"
  lance = gets.chomp.to_i
 return lance 
 end
 
-def jeu (lance)
-    if lance == 5 || lance == 6
-        $marche =+1
-       if $var == 10 
-        puts " Vous avez gagné"
-       
-       else 
-        puts " Vous avez monté une marche #{$var.upto 1} "
-        $var=$marche+1
-
-       end
-    
-    elsif lance == 4 || lance == 3 || lance == 2
-        puts "Vous ne bougez pas" 
-    
-    elsif lance == 1
-        $marche =-1
-        puts "Tu descends d'une marche "
+def jeu (valeurs)
+    if valeurs.to_i == 5 || valeurs.to_i == 6
+        $var = $var + 1
+        puts "Bravo vous avancez d'1 marche, vous êtes sur la marche n° #{$var}"
+    elsif valeurs.to_i==1
+        $var = $var - 1
+        puts "Dommage ! vous descendez d'1 marche; vous êtes sur la marche n° #{$var}"
+    elsif valeurs ==2 || valeurs == 3 || valeurs == 4
+        puts "Vous ne bougez pas, vous restez sur la marche n° #{$var}"
+    else
+        puts "Ce n'est pas une valeur issue d'un dé"
     end
+return $var
 end
 
 def perform
-    jeu (de)
+    $var = 1.to_i
+    coup = 0
+    while $var < 10
+    
+        lance = de
+        jeu (lance)
+
+        coup = coup + 1
+    end
+ "Bravo tu as gagné, tu es sur la marche 10, en #{coup}"
 end
 perform
+
